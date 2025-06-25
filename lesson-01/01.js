b4a3f88b5eb9505a28fa456da108c29553595076
Kostya <gagatyn1337228@mail.ru>
2025-06-25 19:51:06 +0300

    1

--- lesson-01/01.js
+++ lesson-01/01.js
@@ -2,4 +2,11 @@
 // Результат присвойте переменной sum
 
 // your code
-let sum
+let sum = 0;
+let i = 1;
+while (i <= 20){
+    sum = i + sum;
+    i++
+}
+    console.log(sum);
+// sum = 1+2+3+4+5+6+7+8+9+10+11+12+13+14+15+16+17+18+19+20
