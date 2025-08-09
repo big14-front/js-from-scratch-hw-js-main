15247bfef193c9f0ac615a400349e40374fee33b
Kostya <gagatyn1337228@mail.ru>
2025-08-09 12:47:40 +0300

    lesson2-03

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -6,4 +6,16 @@
 
 const score = 70 // тестовое значение, можно изменять
 let grade
+    if(score < 50) {
+        grade = 'F'
+    } else if (score < 70) {
+        grade = 'D'
+    } else if (score < 80) {
+        grade = 'C'
+    } else if (score < 90) {
+        grade = 'B'
+    } else {
+        grade = 'A'
+    }
+
 // your code
